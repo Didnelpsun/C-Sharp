6289f485422a4565a332289a2506b99d719a6073
Didnelpsun <2675350965@qq.com>
2020-11-21 01:09:23 +0800

    更新

--- ATM/Main.Designer.cs
+++ ATM/Main.Designer.cs
@@ -44,7 +44,8 @@ namespace ATM
             this.login.BackColor = System.Drawing.Color.White;
             this.login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.login.FlatAppearance.BorderSize = 0;
-            this.login.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.login.Location = new System.Drawing.Point(209, 227);
             this.login.Margin = new System.Windows.Forms.Padding(0);
             this.login.Name = "login";
@@ -56,52 +57,60 @@ namespace ATM
             // labelUser
             // 
             this.labelUser.AutoSize = true;
-            this.labelUser.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelUser.BackColor = System.Drawing.Color.Transparent;
+            this.labelUser.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelUser.ForeColor = System.Drawing.Color.Black;
             this.labelUser.Location = new System.Drawing.Point(124, 70);
             this.labelUser.Name = "labelUser";
-            this.labelUser.Size = new System.Drawing.Size(69, 19);
+            this.labelUser.Size = new System.Drawing.Size(56, 16);
             this.labelUser.TabIndex = 1;
             this.labelUser.Text = "用户名";
             this.labelUser.Click += new System.EventHandler(this.labelUser_Click);
             // 
             // textBoxUser
             // 
-            this.textBoxUser.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxUser.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBoxUser.Location = new System.Drawing.Point(199, 67);
             this.textBoxUser.Name = "textBoxUser";
-            this.textBoxUser.Size = new System.Drawing.Size(229, 29);
+            this.textBoxUser.Size = new System.Drawing.Size(229, 26);
             this.textBoxUser.TabIndex = 2;
             // 
             // labelPsw
             // 
             this.labelPsw.AutoSize = true;
-            this.labelPsw.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelPsw.BackColor = System.Drawing.Color.Transparent;
+            this.labelPsw.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelPsw.ForeColor = System.Drawing.Color.Black;
             this.labelPsw.Location = new System.Drawing.Point(133, 151);
             this.labelPsw.Name = "labelPsw";
-            this.labelPsw.Size = new System.Drawing.Size(49, 19);
+            this.labelPsw.Size = new System.Drawing.Size(40, 16);
             this.labelPsw.TabIndex = 3;
             this.labelPsw.Text = "密码";
             // 
             // textBoxPsw
             // 
-            this.textBoxPsw.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxPsw.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBoxPsw.Location = new System.Drawing.Point(199, 148);
             this.textBoxPsw.Margin = new System.Windows.Forms.Padding(0);
             this.textBoxPsw.Name = "textBoxPsw";
-            this.textBoxPsw.Size = new System.Drawing.Size(229, 29);
+            this.textBoxPsw.Size = new System.Drawing.Size(229, 26);
             this.textBoxPsw.TabIndex = 4;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(584, 311);
+            this.ControlBox = false;
             this.Controls.Add(this.textBoxPsw);
             this.Controls.Add(this.labelPsw);
             this.Controls.Add(this.textBoxUser);
             this.Controls.Add(this.labelUser);
             this.Controls.Add(this.login);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
---
+++ ATM/MainFunction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM
+{
+    public partial class MainFunction : Form
+    {
+        public MainFunction()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
