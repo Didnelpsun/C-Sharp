c209b84c338239650126ea2595818201aa6bc5b4
Didnelpsun <2675350965@qq.com>
2020-11-30 14:27:11 +0800

    更新

---
+++ ATM/DAO/CheckRepeatUserName.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace ATM
+{
+    partial class DAO
+    {
+        // 返回UserID
+        public static int CheckRepeatUserName(string userID, string userName)
+        {
+            SqlConnection conn = DAO.Connection();
+            // 查询账户是否存在
+            string queryUserName = "SELECT UserID FROM [User] WHERE UserName='" + userName + "'";
+            try
+            {
+                // 打开数据链接
+                conn.Open();
+                //Console.WriteLine("检查用户名{0}是否存在...", user.UserName);
+                // 新建一个命令对象
+                SqlCommand command = conn.CreateCommand();
+                // 写入SQL语句
+                command.CommandText = queryUserName;
+                // 读取对应的结果集
+                SqlDataReader reader = command.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    return 0;
+                }
+                else
+                {
+                    int rows = 0;
+                    string value = "";
+                    while (reader.Read())
+                    {
+                        value = reader["UserID"].ToString();
+                        rows++;
+                    }
+                    if (rows == 1)
+                    {
+                        if(value == userID)
+                        {
+                            reader.Close();
+                            conn.Close();
+                            return 2;
+                        }
+                        else
+                        {
+                            reader.Close();
+                            conn.Close();
+                            return 3;
+                        }
+                    }
+                    else
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return 3;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message.ToString());
+                return 4;
+            }
+        }
+    }
+}
---
+++ ATM/Forms/AddAcount.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ATM.Forms
+{
+    partial class AddAcount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAcount));
+            this.SuspendLayout();
+            // 
+            // AddAcount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(585, 310);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddAcount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddAcount";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
